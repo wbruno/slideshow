e5a6a527f108b55a978a12d62fa26426795d3497
William Bruno <rocha_bruno@hotmail.com>
2014-08-21 16:01:24 -0300

    New effect

--- slideshow.js
+++ slideshow.js
@@ -3,7 +3,7 @@
  * @author William Bruno
  * @date 2013-05-25
  */
-var adcast = (function(window, document) {
+var adcast = (function (window, document) {
     "use strict";
     /*jslint plusplus: true, browser: true */
     var module = {
@@ -11,7 +11,7 @@ var adcast = (function(window, document) {
         atual : 0,
         max   : 1,
         delay : 4000,
-        config : function(config) {
+        config : function (config) {
             module.$adcastWrap  = config.adcasts[0].parentNode;
             module.$adcasts     = [].slice.call(config.adcasts);
             module.$pagers      = [].slice.call(config.pagers);
@@ -20,7 +20,14 @@ var adcast = (function(window, document) {
 
             module.max = module.$adcasts.length;
         },
-        active : function() {
+        infinite : function (i) {
+            var prev = i > 0 ? i - 1 : module.max - 1,
+                next = i < module.max - 1 ? i + 1 : 0;
+
+            module.$adcasts[prev].classList.add('prev');
+            module.$adcasts[next].classList.add('next');
+        },
+        active : function () {
             var i = module.atual;
             module.$adcasts[i].classList.add('is-active');
             module.$pagers[i].classList.add('is-active');
@@ -30,41 +37,41 @@ var adcast = (function(window, document) {
                 module.onChange(i);
             }
         },
-        none : function() {
-            module.$adcasts.forEach(function($adcast) {
-                $adcast.classList.remove('is-active');
-            });
-            module.$pagers.forEach(function($pager) {
-                $pager.classList.remove('is-active');
+        none : function ($els) {
+            $els.forEach(function ($el) {
+                $el.classList.remove('is-active','prev','next');
             });
         },
-        next : function() {
+        next : function () {
             var i = module.atual;
 
-            module.none();
+            module.none(module.$adcasts);
+            module.none(module.$pagers);
             module.active();
+
+            module.infinite(i);
         },
-        _setAtual : function(i) {
+        _setAtual : function (i) {
             if (i < 0) {
                 module.atual = module.max - 1;
             } else {
                 module.atual = i < module.max ? parseInt(i, 10) : 0;
             }
         },
-        _auto : function() {
-            module.timer = window.setInterval(function() {
+        _auto : function () {
+            module.timer = window.setInterval(function () {
                 module._setAtual(module.atual + 1);
                 module.next();
             }, module.delay);
         },
-        _pause: function() {
+        _pause: function () {
             window.clearInterval(module.timer);
             window.clearTimeout(module.tot);
         },
-        _onPagersClick : function($pager) {
-            $pager.addEventListener('click', function() {
+        _onPagersClick : function ($pager) {
+            $pager.addEventListener('click', function () {
                 var $this = this,
-                    i = $this.getAttribute('data-adcast');
+                    i = parseInt($this.getAttribute('data-adcast'), 10);
 
                 module._pause();
                 module.tot = window.setTimeout(module.auto, module.delay / 2);
@@ -73,26 +80,25 @@ var adcast = (function(window, document) {
                 module.next();
             });
         },
-        _onPagersHover : function($pager) {
-            $pager.addEventListener('mouseover', function() {
+        _onPagersHover : function ($pager) {
+            $pager.addEventListener('mouseover', function () {
                 var $this = this,
-                    i = $this.getAttribute('data-adcast');
+                    i = parseInt($this.getAttribute('data-adcast'), 10);
 
                 module.atual = i;
                 window.clearInterval(module.timer);
-                module.none();
-                module.active();
+                module.next();
             });
-            $pager.addEventListener('mouseout', function() {
+            $pager.addEventListener('mouseout', function () {
                 var $this = this,
-                    i = $this.getAttribute('data-adcast');
+                    i = parseInt($this.getAttribute('data-adcast'), 10);
 
                 module._setAtual(i);
                 module.auto();
             });
         },
-        _keyboard : function() {
-            document.addEventListener('keydown', function(event){
+        _keyboard : function () {
+            document.addEventListener('keydown', function (event){
                 var i;
                 if(event.keyCode == 39) { //right
                     i = module.atual + 1;
@@ -105,8 +111,8 @@ var adcast = (function(window, document) {
                 module.next();
             });
         },
-        events : function() {
-            module.$pagers.forEach(function($pager) {
+        events : function () {
+            module.$pagers.forEach(function ($pager) {
                 if (module.pagersClick) {
                     module._onPagersClick($pager);
                 } else {
@@ -115,12 +121,10 @@ var adcast = (function(window, document) {
             });
             module._keyboard();
         },
-        init : function(config) {
+        init : function (config) {
             module.config(config);
 
-            if (!module.max || module.max === 1) {
-                return;
-            }
+            if (!module.max || module.max === 1) return;
 
             module.next();
             if (config.auto) module._auto();
